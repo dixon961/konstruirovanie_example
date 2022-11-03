b1ee3a7505c1b408d2e4fa6f7990c43618228100
Alexey Markov <dixon96143@gmail.com>
2022-11-03 11:25:03 +0300

    Добавил логику в main

--- app/src/main/java/konstruirovanie_example/App.java
+++ app/src/main/java/konstruirovanie_example/App.java
@@ -3,5 +3,9 @@ package konstruirovanie_example;
 public class App {
 
     public static void main(String[] args) {
+        var doctor = new Doctor("1", "Ivan Ivanov");
+        var user = new User("2", "Petr Talanov");
+        System.out.println("Doctor: " + doctor.fullName());
+        System.out.println("User: " + user.fullName());
     }
 }
